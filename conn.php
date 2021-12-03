d9ef3c106ee7b3dde62a03dfc58bfc875440a207
techhans <92838352+techhans@users.noreply.github.com>
2021-12-03 19:22:06 +0900

    Update conn.php
    commit hidden 2

--- conn.php
+++ conn.php
@@ -1,12 +1,12 @@
 <?php
 	$db_username = 'test';
 	$db_password = 'test';
-//	$conn = new PDO( 'mysql:host=localhost;dbname=test_db', $db_username, $db_password );
+	$conn = new PDO( 'mysql:host=localhost;dbname=test_db', $db_username, $db_password );
 
 
 
 	if(!$conn){
-		die("Fatal Error: Connection Failed!");
+		die("Fatal Error: Connection Failed!");https://github.com/techhans/php_project_01/blob/main/conn.php
 	}else{
 		echo "
 		<script>alert('database connected!')</script>
