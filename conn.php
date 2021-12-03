116ca2c85f09bcf68c81de2ce893f71cff54c256
techhans2019 <techhans2019@gmail.com>
2021-12-03 19:34:13 +0900

    test

---
+++ conn.php
@@ -0,0 +1,18 @@
+<?php
+	$db_username = 'test';
+	$db_password = 'test';
+	$conn = new PDO( 'mysql:host=localhost;dbname=test_db', $db_username, $db_password );
+
+
+
+	if(!$conn){
+		die("Fatal Error: Connection Failed!");
+	}else{
+		echo "
+		<script>alert('database connected!')</script>
+		";
+
+	}
+
+
+?>
