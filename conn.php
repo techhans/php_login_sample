45f020928e77b7c9fea5db2931afe294724ec29f
techhans2019 <techhans2019@gmail.com>
2021-12-03 19:25:47 +0900

    default

--- conn.php
+++ conn.php
@@ -1,9 +1,8 @@
 <?php
 	$db_username = 'test';
 	$db_password = 'test';
-//	$conn = new PDO( 'mysql:host=localhost;dbname=test_db', $db_username, $db_password );
-	$conn = new PDO( 'mysql:host=152.67.203.84;dbname=test_db', $db_username, $db_password );
-//	$conn = new PDO( 'mysql:host=152.69.226.20;dbname=test_db', $db_username, $db_password );
+	$conn = new PDO( 'mysql:host=localhost;dbname=test_db', $db_username, $db_password );
+
 
 
 	if(!$conn){
